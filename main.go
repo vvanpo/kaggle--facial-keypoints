255ad6fb4b37631d7f2cb5c673a112c0eeb67593
Victor van Poppelen <vvanpo@gmail.com>
2014-03-29 19:16:25 -0400

    Incorrect implementation of in-memory pixbuf creation.  Pixels don't line up for unknown reason, could be in this package or in the SetPixel() implementation.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-    "fmt"
     "os"
     "log"
     "image"
@@ -11,6 +10,31 @@ import (
     "github.com/conformal/gotk3/gdk"
     "github.com/conformal/gotk3/gtk"
 )
+
+func ImageToPixbuf(img image.Image) (*gdk.Pixbuf, error) {
+    width := img.Bounds().Max.X - img.Bounds().Min.X
+    height := img.Bounds().Max.Y - img.Bounds().Min.Y
+    pixbuf, err := gdk.PixbufNew(gdk.COLORSPACE_RGB, true, 8, width, height)
+    if err != nil {
+        return nil, err
+    }
+    pix := make([]byte, width * height * 4)
+    for y := 0; y < height; y++ {
+        for x := 0; x < width; x++ {
+            i := ((x * y) + x) * 4
+            r, g, b, a := img.At(x, y).RGBA()
+            pix[i] = byte(r / 256)
+            pix[i + 1] = byte(g / 256)
+            pix[i + 2] = byte(b / 256)
+            pix[i + 3] = byte(a / 256)
+        }
+    }
+    err = pixbuf.SetPixels(pix)
+    if err != nil {
+        return nil, err
+    }
+    return pixbuf, nil
+}
 
 func main() {
 
@@ -25,15 +49,14 @@ func main() {
             log.Print("Failed to decode image ", file)
             continue
         }
-        width := img.Bounds().Max.X - img.Bounds().Min.X
-        height := img.Bounds().Max.Y - img.Bounds().Min.Y
-        fmt.Printf("(%d, %d)\n", width, height)
-        // Initialize a Pixbuf to copy img into
-        pb, err := gdk.PixbufNew(gdk.COLORSPACE_RGB, false, 8, width, height)
+
+        // Copy img into a new gdk.Pixbuf
+        pb, err := ImageToPixbuf(img)
         if err != nil {
             log.Print("Failed to create a pixbuf for image ", file)
             continue
         }
+
         // Initialize GTK window
         gtk.Init(nil)
         win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
