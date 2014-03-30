a391ce4ee5f09fdfc915d87ac4355947ba2c317c
Victor van Poppelen <vvanpo@gmail.com>
2014-03-30 05:18:06 -0400

    Loads PGM from test.csv, decodes to image.Image, encodes to gdk.Pixbuf, the displays in Gtk window.  When window is closed by user the cycle repeats with the next record from test.csv.

--- load.go
+++ load.go
@@ -3,8 +3,12 @@ package main
 
 import (
     "os"
+    "io"
     "io/ioutil"
     "log"
+    "image"
+    "strings"
+    "strconv"
     "encoding/csv"
     "gopkg.in/yaml.v1"
 )
@@ -26,11 +30,42 @@ func init() {
     }
 }
 
-func loadTest() {
+func loadTest(out chan image.Image) {
+    defer close(out)
     f, err := os.Open(config["test.csv"])
     if err != nil {
-        log.Fatal(err)
+        log.Print(err)
+        return
     }
-    r := csv.NewReader(f)
-    print(r)
+    test := csv.NewReader(f)
+    test.TrimLeadingSpace = true
+    for {
+        record, err := test.Read()
+        if err == io.EOF {
+            break
+        } else if err != nil {
+            log.Print(err)
+            return
+        }
+        if record[1] == "Image" { continue }
+        img, err := decodePGM(record[1])
+        if err != nil {
+            log.Print(err)
+            return
+        }
+        out <- img
+    }
+}
+
+func decodePGM(pgm string) (image.Image, error) {
+    img := image.NewGray(image.Rect(0, 0, 96, 96))
+    f := strings.Fields(pgm)
+    for i, v := range f {
+        n, err := strconv.Atoi(v)
+        img.Pix[i] = uint8(n)
+        if err != nil {
+            return nil, err
+        }
+    }
+    return img, nil
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-    "os"
     "log"
     "image"
     _ "image/png"
@@ -10,21 +9,12 @@ import (
 )
 
 func main() {
-
-    for _, file := range os.Args[1:] {
-        f, err := os.Open(file)
+    img := make(chan image.Image)
+    go loadTest(img)
+    for {
+        err := displayImage(<-img)
         if err != nil {
-            log.Print("Failed to open file ", file)
-            continue
-        }
-        img, _, err := image.Decode(f)
-        if err != nil {
-            log.Print("Failed to decode image ", file)
-            continue
-        }
-        err = displayImage(img)
-        if err != nil {
-            log.Print(err)
+            log.Fatal(err)
         }
     }
 }
