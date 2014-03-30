11b517c3bed56a53154cf20d421d8e691c6905ba
Victor van Poppelen <vvanpo@gmail.com>
2014-03-29 20:40:00 -0400

    Fixed GdkPixbuf misalignment issue

--- main.go
+++ main.go
@@ -11,33 +11,54 @@ import (
     "github.com/conformal/gotk3/gtk"
 )
 
-func ImageToPixbuf(img image.Image) (*gdk.Pixbuf, error) {
+// Copies image to a new GdkPixbuf
+func imageToPixbuf(img image.Image) (*gdk.Pixbuf, error) {
     width := img.Bounds().Max.X - img.Bounds().Min.X
     height := img.Bounds().Max.Y - img.Bounds().Min.Y
     pixbuf, err := gdk.PixbufNew(gdk.COLORSPACE_RGB, true, 8, width, height)
     if err != nil {
         return nil, err
     }
-    pix := make([]byte, width * height * 4)
+    pix := pixbuf.GetPixels()
     for y := 0; y < height; y++ {
         for x := 0; x < width; x++ {
-            i := ((x * y) + x) * 4
+            i := ((width * y) + x) * 4
             r, g, b, a := img.At(x, y).RGBA()
-            pix[i] = byte(r / 256)
-            pix[i + 1] = byte(g / 256)
-            pix[i + 2] = byte(b / 256)
-            pix[i + 3] = byte(a / 256)
+            pix[i] = byte(r)
+            pix[i + 1] = byte(g)
+            pix[i + 2] = byte(b)
+            pix[i + 3] = byte(a)
         }
-    }
-    err = pixbuf.SetPixels(pix)
-    if err != nil {
-        return nil, err
     }
     return pixbuf, nil
 }
 
-func main() {
+// Displays image in a new Gtk window
+// This function blocks until the window is closed
+func displayImage(img image.Image) {
+        // Copy img into a new gdk.Pixbuf
+        pixbuf, err := imageToPixbuf(img)
+        if err != nil {
+            log.Print("Failed to create a pixbuf for image")
+        }
+        // Initialize GtkWindow
+        gtk.Init(nil)
+        win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+        if err != nil {
+            log.Print("Failed to create new window")
+        }
+        win.Connect("destroy", func() { gtk.MainQuit() })
+        // Create GtkImage widget from pixbuf
+        gimg, err := gtk.ImageNewFromPixbuf(pixbuf)
+        if err != nil {
+            log.Print("Failed to load image into window")
+        }
+        win.Add(gimg)
+        win.ShowAll()
+        gtk.Main()
+}
 
+func main() {
     for _, file := range os.Args[1:] {
         f, err := os.Open(file)
         if err != nil {
@@ -49,22 +70,6 @@ func main() {
             log.Print("Failed to decode image ", file)
             continue
         }
-
-        // Copy img into a new gdk.Pixbuf
-        pb, err := ImageToPixbuf(img)
-        if err != nil {
-            log.Print("Failed to create a pixbuf for image ", file)
-            continue
-        }
-
-        // Initialize GTK window
-        gtk.Init(nil)
-        win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-        win.Connect("destroy", func() { gtk.MainQuit() })
-        i, err := gtk.ImageNewFromPixbuf(pb)
-        win.Add(i)
-        win.ShowAll()
-        gtk.Main()
+        displayImage(img)
     }
-
 }
